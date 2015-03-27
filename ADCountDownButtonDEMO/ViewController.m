53abc9ab0d6fc60baf624a5aa41f25a11ae0ef9e
CuiPanJun <cuipanjun@mmbang.net>
2015-03-27 13:57:37 +0800

    add screenshot

--- ADCountDownButtonDEMO/ViewController.m
+++ ADCountDownButtonDEMO/ViewController.m
@@ -19,9 +19,42 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    ADCountDownButton *countButton = [[ADCountDownButton alloc] initWithFrame:CGRectMake(30, 100, 60, 21)];
+    ADCountDownButton *countButton = [[ADCountDownButton alloc] initWithFrame:CGRectMake(60, 100, 60, 21)];
     countButton.backgroundColor = [UIColor yellowColor];
     CALayer *layer = countButton.layer;
+    layer.cornerRadius = 10;
+    countButton.titleLabel.font = [UIFont boldSystemFontOfSize:12];
+    countButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+    
+    [countButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [countButton setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
+    [countButton setTitle:@"验证码" forState:UIControlStateNormal];
+    countButton.totalTime = 6;
+    countButton.didStartBlock = ^(ADCountDownButton *abutton){
+        NSString *timeLeftString = [NSString stringWithFormat:@"重新发送(%0.f)",abutton.secondsLeft];
+        [abutton setTitle:timeLeftString forState:UIControlStateNormal];
+        [abutton setTitle:timeLeftString forState:UIControlStateDisabled];
+    };
+    countButton.updatedBlock = ^(ADCountDownButton *abutton){
+        NSString *timeLeftString = [NSString stringWithFormat:@"重新发送(%0.f)",abutton.secondsLeft];
+        [abutton setTitle:timeLeftString forState:UIControlStateNormal];
+        [abutton setTitle:timeLeftString forState:UIControlStateDisabled];
+    };
+    countButton.finishedBlock = ^(ADCountDownButton *abutton, BOOL finished){
+        NSString *titleString = @"验证码";
+        [abutton setTitle:titleString forState:UIControlStateNormal];
+        [abutton setTitle:titleString forState:UIControlStateDisabled];
+        [abutton reset];
+    };
+    
+    [countButton addTarget:self action:@selector(getCaptcha:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    [self.view addSubview:countButton];
+    
+    countButton = [[ADCountDownButton alloc] initWithFrame:CGRectMake(60, 180, 60, 21)];
+    countButton.backgroundColor = [UIColor yellowColor];
+    layer = countButton.layer;
     layer.cornerRadius = 10;
     countButton.titleLabel.font = [UIFont boldSystemFontOfSize:12];
     countButton.titleLabel.adjustsFontSizeToFitWidth = YES;
